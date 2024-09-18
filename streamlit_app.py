8b93c8043b6e93a5a1791fe7c754f3fa874d1bd2
Pavithren V S Pakianathan <pavinod@msn.com>
2024-09-18 14:45:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,4 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.write("Have a nice day!")
+st.set_page_config(page_title='Data Sensemaking Demo', page_icon=':smile', layout="centered", initial_sidebar_state="auto", menu_items=None)
