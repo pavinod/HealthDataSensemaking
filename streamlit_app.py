769fff00604890e31553dbc72c50f063e0a2e7dc
Pavithren V S Pakianathan <pavinod@msn.com>
2024-09-18 14:48:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
+st.set_page_config(page_title = "Data Sensemaking Demo")
 st.write("I will complete by PhD successfully by September 2025")
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.write("Have a nice day!")
-st.set_page_config(page_title='Data Sensemaking Demo')
