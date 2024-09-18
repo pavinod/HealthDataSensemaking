6704ba1dc632c798a1d6295d92e93bef7ee2cd9c
Pavithren V S Pakianathan <pavinod@msn.com>
2024-09-18 14:41:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+st.write("I will complete by PhD successfully by September 2025")
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
