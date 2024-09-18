83a95dacf00d9415f9f9a8938cc8ec3069374d58
Pavithren V S Pakianathan <pavinod@msn.com>
2024-09-18 15:22:23 +0200

    caption

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.set_page_config(page_title = "Data Sensemaking Demo")
-st.write("I will complete by PhD successfully by September 2025")
-st.title("🎈 My new app")
+st.title("I will complete by PhD successfully by September 2025")
+st.caption("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
