0e1710238a431eca27ab07dde5a01f67400edf39
Pavithren V S Pakianathan <pavinod@msn.com>
2024-09-19 16:39:57 +0200

    ydata

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from datetime import time, datetime
 import pandas as pd
 import ydata_profiling
-from ydata_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
 import numpy as np
 
 #https://pavithren.streamlit.app/
